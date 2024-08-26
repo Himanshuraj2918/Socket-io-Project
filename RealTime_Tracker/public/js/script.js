4a0cdbe21563b48f8d0e2a64e50d66c020556a8c
Himanshu2918 <himanshuraj2918@gmail.com>
2024-08-26 11:35:35 +0530

    navigator function

--- RealTime_Tracker/public/js/script.js
+++ RealTime_Tracker/public/js/script.js
@@ -1,5 +1,15 @@
 const socket = io();
 
 if(navigator.geolocation){
-    
+    navigator.geolocation.watchPosition((poistion)=>{
+            const {latitude,longitude} = poistion.coords;
+            socket.emit("send-location",{latitude,longitude})
+    }, (error)=>{
+        console.error(error);
+        
+    },{
+        enableHighAccuracy:true,
+        timeout:5000,
+        maximumAge:0,
+    })
 }
