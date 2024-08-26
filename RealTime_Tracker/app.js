20308985f5353814e1dcb53712f4d855f27a1ae0
Himanshu2918 <himanshuraj2918@gmail.com>
2024-08-26 11:30:07 +0530

    server update

--- RealTime_Tracker/app.js
+++ RealTime_Tracker/app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require('path')
 const app = express();
 
 const http = require("http")
@@ -6,8 +7,15 @@ const socket = require("socket.io")
 const server = http.createServer(app)
 const io = socket(server)
 
+app.set("view engine","ejs")
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection",(socket)=>{
+    console.log("connected");
+    
+})
 app.get("/",(req,res)=>{
-    res.send("hey")
+    res.render("index")
 })
 
 server.listen(8000,()=>{
